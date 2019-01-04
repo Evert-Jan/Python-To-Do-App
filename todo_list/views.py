3f317862e57e080389148709b35398e414bce5a0
Evert-Jan <38124964+Evert-Jan@users.noreply.github.com>
2019-01-04 21:31:15 +0100

    Add files via upload

--- todo_list/views.py
+++ todo_list/views.py
@@ -5,56 +5,56 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 
 def home(request):
-
 	if request.method == 'POST':
 		form = ListForm(request.POST or None)
-        
+
 		if form.is_valid():
 			form.save()
 			all_items = List.objects.all
-			messages.success(request, ('Item Has Been Added To List!'))
-			return render(request, 'home.html', {'all_items': all_items})
+			messages.success(request, ('Item Has Been Added To List'))
+			context = {'all_item': all_items}
+			return render(request, 'home.html', context)
 
 	else:
 		all_items = List.objects.all
-		return render(request, 'home.html', {'all_items': all_items})
+		context = {'all_item': all_items}
+		return render(request, 'home.html', context)
 
 def about(request):
-	context = {'first_name': 'John', 'last_name': 'Elder'}
+	voornaam = 'Evert-Jan'; achternaam = 'Lohnstein'
+	context = {'first_name': voornaam, 'last_name': achternaam }
 	return render(request, 'about.html', context)
 
 def delete(request, list_id):
 	item = List.objects.get(pk=list_id)
 	item.delete()
-	messages.success(request, ('Item Has Been Deleted!'))
+	messages.success(request, ('Item Has Been deleted!'))
 	return redirect('home')
 
 def cross_off(request, list_id):
 	item = List.objects.get(pk=list_id)
 	item.completed = True
 	item.save()
-	return redirect('home')	
+	return redirect('home')
 
 def uncross(request, list_id):
 	item = List.objects.get(pk=list_id)
 	item.completed = False
 	item.save()
-	return redirect('home')	
+	return redirect('home')
 
 def edit(request, list_id):
 	if request.method == 'POST':
-		item = List.objects.get(pk=list_id)
+		edititem = List.objects.get(pk=list_id)
+		form = ListForm(request.POST or None, instance=edititem)
 
-		form = ListForm(request.POST or None, instance=item)
-        
 		if form.is_valid():
 			form.save()
-			messages.success(request, ('Item Has Been Edited!'))
+			messages.success(request, ('Item Has Been Edited to'))
 			return redirect('home')
 
 	else:
-		item = List.objects.get(pk=list_id)
-		return render(request, 'edit.html', {'item': item})
-
-
+		edititem = List.objects.get(pk=list_id)
+		context = {'edititem': edititem}
+		return render(request, 'edit.html', context)
 
